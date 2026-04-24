9ed76fe8dde35c3edbee3cd8914eae936d74f264
RobinSigmaBlomqvist <robin.blomqvist@elev.ga.ntig.se>
2026-04-24 09:39:33 +0200

    skib

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ board = []
 
 rätt = 0
 badpoints = 0
-place = True
+
 for x in range(0,5):
     board.append(["O"] * 5)
 
@@ -19,14 +19,14 @@ def random_row(board):
 
 def random_col(board):
     return random.randint(0, len(board[0]) - 1)
-while place==True:
+while True:
     ship1_row = random_row(board)
     ship1_col = random_col(board)
     ship2_row = ship1_row + random.randint(-1,1)
     ship2_col = ship1_col + random.randint(-1,1)
-    
+
     if ship2_row >= 1 and ship2_row <= 5 and ship2_col >= 1 and ship2_col <= 5:
-        place == False
+        break
 
 while True:
     guess_row = int(input("Guess Row:"))-1
@@ -51,11 +51,16 @@ while True:
     if rätt >= 2:
         if badpoints>=20:
             print("Du suger")
+            break
         elif badpoints >= 11:
             print("Du van, men dåligt")
+            break
         elif badpoints == 6 or badpoints ==7:
             print("SIX or SEVEN")
+            break
         elif badpoints >=4:
             print("Die vinnen, guten pojken")
+            break
         else:
             print("Wow skib-maister du äger")
+            break
