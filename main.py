090c0ba2e91be1c5ae09bafb8b09e220aeb495ad
RobinSigmaBlomqvist <robin.blomqvist@elev.ga.ntig.se>
2026-05-08 09:38:53 +0200

    fixat och storlek

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import random
 import os
 board = []
 
-rätt = 0
+skep1träff = False
+skep2träff = False
 badpoints = 0
+storlek = int(input("Hur stor karta vill du spela? (rekomenderat 5 max 61 helst)"))
 
-for x in range(0,5):
-    board.append(["O"] * 5)
+for x in range(0,storlek):
+    board.append(["O"] * storlek)
 
 def print_board(board):
     for row in board:
@@ -19,17 +21,14 @@ def random_row(board):
 
 def random_col(board):
     return random.randint(0, len(board[0]) - 1)
+
 while True:
-    ship1_row = random_row(board) + 1
-    ship1_col = random_col(board) + 1
+    ship1_row = random_row(board)
+    ship1_col = random_col(board)
     ship2_row = ship1_row + random.randint(-1,1)
     ship2_col = ship1_col + random.randint(-1,1)
 
-    if ship2_row >= 1 and ship2_row <= 5 and ship2_col >= 1 and ship2_col <= 5 and (ship2_row != ship1_row or ship2_col != ship1_col):
-        print(ship1_col)
-        print(ship1_row)
-        print(ship2_col)
-        print(ship2_row)
+    if ship2_row >= 0 and ship2_row <= len(board) - 1 and ship2_col >= 0 and ship2_col <= len(board) - 1 and (ship2_row != ship1_row or ship2_col != ship1_col):
         break
 
 while True:
@@ -41,29 +40,30 @@ while True:
         board[guess_row][guess_col] = '+'
         print_board(board)
         print("Wow du träffa mitt skäpp!")
-        rätt += 1
+        skep1träff = True
     elif guess_row == ship2_row and guess_col == ship2_col:
         board[guess_row][guess_col] = '+'
         print_board(board)
         print("Wow du träffa mitt skäpp!")
-        rätt += 1
+        skep2träff = True
     else:
         board[guess_row][guess_col] = 'X'
         print_board(board)
         print("You missed my battleship!")
         badpoints += 1
-    if rätt >= 2:
+
+    if skep1träff == True and skep2träff == True:
         if badpoints>=20:
             print("Du suger")
             break
         elif badpoints >= 11:
-            print("Du van, men dåligt")
+            print("Du van")
             break
         elif badpoints == 6 or badpoints ==7:
             print("SIX or SEVEN")
             break
         elif badpoints >=4:
-            print("Die vinnen, guten pojken")
+            print("Du vinnen, guten pojken")
             break
         else:
             print("Wow skib-maister du äger")
